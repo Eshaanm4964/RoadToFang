8f6bdba5eda32fd99ab23f74ab16ef281c917703
Eshaanm4964 <eshaanm4964@gmail.com>
2026-06-27 10:51:47 +0530

    Initial commit 27-06-2026

---
+++ BinarySearch/search_rotated_sorted_array.py
@@ -0,0 +1,25 @@
+def search_rotated_sorted_array(nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
+nums = [4,5,6,7,0,1,2]
+target = 0
+print(search_rotated_sorted_array(nums,target))
+
