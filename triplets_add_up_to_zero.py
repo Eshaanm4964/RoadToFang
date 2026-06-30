947962d12ba571b135942c6e45319065c6f17d7b
Eshaanm4964 <eshaanm4964@gmail.com>
2026-06-30 12:02:53 +0530

    Initial commit 30-06-2026

---
+++ triplets_add_up_to_zero.py
@@ -0,0 +1,14 @@
+def triplets_sum_zer0(arr):
+    n = len(arr)
+    
+    st = set()
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            for k in range(j+1,len(arr)):
+                if arr[i]+arr[j]+arr[k]==0:
+                    triplet = tuple(sorted([arr[i], arr[j], arr[k]]))
+                    st.add(triplet)
+    return [list(triplet) for triplet in st]
+
+arr = [-1,0,1,2,-1,4]
+print(triplets_sum_zer0(arr))
