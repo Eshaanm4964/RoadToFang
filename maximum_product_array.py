fc714f020453fec3b0cc80bd861fa1cd8564354e
Eshaanm4964 <eshaanm4964@gmail.com>
2026-07-01 10:21:06 +0530

    Second commit 01-07-2026

--- maximum_product_array.py
+++ maximum_product_array.py
@@ -1,7 +1,11 @@
 def max_product(arr):
     n = len(arr)
-    arr.sort()
-    return arr[-1]*arr[-2]*arr[-3]
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr, arr[-1]*arr[-2]*arr[-3]
 
 arr = [10,3,5,6,20]
 print(max_product(arr))
